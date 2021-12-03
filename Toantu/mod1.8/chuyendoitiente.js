97e3079936f65832300a72821d79461defbdce54
Jack King <ledoanquoc12c3@gmail.com>
2021-12-03 13:45:50 +0700

    chuyen doi tien te

--- Toantu/mod1.8/chuyendoitiente.js
+++ Toantu/mod1.8/chuyendoitiente.js
@@ -1,11 +1,11 @@
-var money = document.getElementById('amount').value;
-var country1 = document.getElementById('goc1').value;
-var country2 = document.getElementById('goc2').value;
-var usMoney = money/23000;
-var vnMoney = money*23000;
+
 function convert(){
-    
-    if(country1==='vn' && country2==='us'){
+    var money = document.getElementById('amount').value;
+    var country1 = document.getElementById('goc1').value;
+    var country2 = document.getElementById('goc2').value;
+    var usMoney = money/23000;
+    var vnMoney = money*23000;
+if(country1==='vn' && country2==='us'){
         document.getElementById('result').innerHTML = 'Result: ' + "$" + usMoney;
     } 
     else if (country1==='us' && country2 === 'vn'){
