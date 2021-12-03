021c662f0a30151ef1594ebb45170d7b688cc035
Jack King <ledoanquoc12c3@gmail.com>
2021-12-03 13:43:13 +0700

    chuyen doi tien te

---
+++ Toantu/mod1.8/chuyendoitiente.js
@@ -0,0 +1,17 @@
+var money = document.getElementById('amount').value;
+var country1 = document.getElementById('goc1').value;
+var country2 = document.getElementById('goc2').value;
+var usMoney = money/23000;
+var vnMoney = money*23000;
+function convert(){
+    
+    if(country1==='vn' && country2==='us'){
+        document.getElementById('result').innerHTML = 'Result: ' + "$" + usMoney;
+    } 
+    else if (country1==='us' && country2 === 'vn'){
+        document.getElementById('result').innerHTML = 'Result: ' +  vnMoney + 'VND';
+    } 
+    else{
+        alert('Hay chon loai tien muon quy doi');
+    }
+}
